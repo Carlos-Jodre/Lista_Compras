2a75c12bb37089fb97569b3f0f6e36f6bb1bf272
Carlos-Jodre <ingeproyectos.chile@gmail.com>
2026-06-28 16:34:58 +0200

    Update Firebase configuration details

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCT6LrlykJaC-YVSIcIn28pEQeznoV9G4",
-  authDomain: "lista-compras-jm.firebaseapp.com",
-  projectId: "lista-compras-jm",
-  storageBucket: "lista-compras-jm.firebasestorage.app",
-  messagingSenderId: "541192537878",
-  appId: "1:541192537878:web:910b4f6b57429de6d1b237"
+apiKey: "AIzaSyDfq4oVaFxhGaqipx52itBxUARcajJhHfA",
+authDomain: "tareas-casa-11c7b.firebaseapp.com",
+projectId: "tareas-casa-11c7b",
+storageBucket: "tareas-casa-11c7b.firebasestorage.app",
+messagingSenderId: "956111451974",
+appId: "1:956111451974:web:aab85979e536d5770e1747"
 };
 
 const app = initializeApp(firebaseConfig);
